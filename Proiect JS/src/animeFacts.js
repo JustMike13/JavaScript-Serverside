cdfdf241b82ac229d039b9ae52007df6c600ec93
JustMike13 <62668963+JustMike13@users.noreply.github.com>
2021-10-19 22:21:32 +0300

    add request to Studio Ghibli API
    add a request that gets the title, banner and description of Studio Ghibli movies from the API

---
+++ Proiect JS/src/animeFacts.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+const animeFacts = async (req, res) => {
+    const response = await fetch('https://ghibliapi.herokuapp.com/films/')
+    const body = await response.json();
+    const facts = body.map(({ title, movie_banner, description}) => ({
+        title,
+        movie_banner,
+        description,
+    }));
+
+    res.send(facts);
+}
+
+module.exports= animeFacts;
--- Proiect JS/src/index.js
+++ Proiect JS/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { handleGreeting, otherValue } = require('./greeting');
 const handleCatFactsRequest = require('./catFacts');
+const handleAnimeFactsRequest = require('./animeFacts');
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,7 @@ app.get('/hello/:name?',(req, res) => {
 });
 
 app.get('/cat/facts', handleCatFactsRequest);
+app.get('/anime/facts', handleAnimeFactsRequest);
 
 app.listen(port, () => {
     console.log("Server started on", port);
